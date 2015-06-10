17b22b16907029bffa37bca0f6c315d6fcdd7451
Ivan Smirnov <i.s.smirnov@gmail.com>
2015-06-10 13:13:09 +0100

    CLI now accepts both directories and pytest positional args (separated by --)
    Also, make --ext a bit smarter by prepending the '.' automatically if need be.

--- pytest_watch/command.py
+++ pytest_watch/command.py
@@ -8,13 +8,11 @@ All positional arguments are passed directly to py.test executable.
 
 
 Usage:
-  ptw [options] [--] [<args>...]
+  ptw [options] [<directories>...] [-- <args>...]
 
 Options:
   -h --help         Show this help.
   --version         Show version.
-  --watch=<dirs>    Command-separated list of directories to be watched recursively
-                    (default: current directory).
   --ignore=<dirs>   Command-separated list of directories to ignore when descending
                     (if relative: starting from the root of each watched dir).
   -c --clear        Automatically clear the screen before each run.
@@ -39,18 +37,29 @@ def main(argv=None):
     """The entry point of the application."""
     colorama.init()
 
-    if argv is None:
-        argv = sys.argv[1:]
-    usage = '\n\n\n'.join(__doc__.split('\n\n\n')[1:])
+    usage = __doc__[__doc__.find('Usage:'):]
     version = 'pytest-watch ' + __version__
-
+    argv = argv if argv is not None else sys.argv[1:]
     args = docopt(usage, argv=argv, version=version)
-    return watch(directories=(args['--watch'] or '').split(','),
-                 ignore=(args['--ignore'] or '').split(','),
+
+    directories, pytest_args = args['<directories>'], []
+    if '--' in directories:
+        ix = directories.index('--')
+        directories, pytest_args = directories[:ix], directories[(ix + 1):]
+
+    ignore = (args['--ignore'] or '').split(',')
+    extensions = [
+        '.' * (not ext.startswith('.')) + ext
+        for ext in (args['--ext'] or '.py').split(',')
+    ]
+    print(ignore, extensions)
+
+    return watch(directories=directories,
+                 ignore=ignore,
                  auto_clear=args['--clear'],
                  beep_on_failure=not args['--nobeep'],
                  onpass=args['--onpass'],
                  onfail=args['--onfail'],
                  poll=args['--poll'],
-                 extensions=(args['--ext'] or '.py').split(','),
-                 args=args['<args>'])
+                 extensions=extensions,
+                 args=pytest_args)
