efebe20376084692d66ea99a0a9af8802c77103d
Andre Pastore <pastore@mondli.com>
2018-01-02 19:52:10 -0200

    Refactor basic tests with default arguments. Added a new test for test_default_command

--- pytest_watch/tests/test_main.py
+++ pytest_watch/tests/test_main.py
@@ -1,41 +1,54 @@
+import sys
 import unittest
-
-from pytest_watch.command import main
+import shutil
+import tempfile
 
 try:
     from unittest.mock import patch
 except ImportError:
     from mock import patch
 
+from pytest_watch.command import main
+
 
 class TestCLIArguments(unittest.TestCase):
 
+    def _get_default_args(self):
+        return dict(
+                directories=[],
+                ignore=[],
+                extensions=[".py"],
+                beep_on_failure=True,
+                auto_clear=False,
+                wait=False,
+                beforerun=None,
+                afterrun=None,
+                onpass=None,
+                onfail=None,
+                onexit=None,
+                runner=None,
+                spool=200,
+                poll=False,
+                verbose=False,
+                quiet=False,
+                pytest_args=[]
+            )
+
     @patch("pytest_watch.command.watch", side_effect=lambda *args, **kwargs: 0)
     def test_default_parameters(self, watch_callee):
-        default_args = dict(
-            directories=[],
-            ignore=[],
-            extensions=None,
-            beep_on_failure=True,
-            auto_clear=False,
-            wait=False,
-            beforerun=None,
-            afterrun=None,
-            onpass=None,
-            onfail=None,
-            onexit=None,
-            runner=None,
-            spool=200,
-            poll=False,
-            verbose=False,
-            quiet=False,
-            pytest_args=[]
-        )
-
         main([])
 
         watch_callee.assert_called_once()
-        watch_callee.assert_called_once_with(**default_args)
+        watch_callee.assert_called_once_with(**self._get_default_args())
+
+    @patch("pytest_watch.command.watch", side_effect=lambda *args, **kwargs: 0)
+    def test_empty_argv(self, watch_callee):
+        sys.argv[1:] = []
+
+        main()
+
+        watch_callee.assert_called_once()
+        watch_callee.assert_called_once_with(**self._get_default_args())
 
     @patch("pytest_watch.command.watch", side_effect=lambda *args, **kwargs: 0)
     def test_zero_spool_value(self, watch_callee):
@@ -47,6 +60,7 @@ class TestCLIArguments(unittest.TestCase):
     @patch("pytest_watch.command.watch", side_effect=lambda *args, **kwargs: 0)
     def test_positive_spool_value(self, watch_callee):
         main("--spool 2000".split())
+
         self.assertIn("spool", watch_callee.call_args[1])
         self.assertEqual(2000, watch_callee.call_args[1]["spool"])
         watch_callee.assert_called_once()
@@ -54,6 +68,7 @@ class TestCLIArguments(unittest.TestCase):
         watch_callee.reset_mock()
 
         main("--spool 20".split())
+
         self.assertIn("spool", watch_callee.call_args[1])
         self.assertEqual(20, watch_callee.call_args[1]["spool"])
         watch_callee.assert_called_once()
@@ -61,6 +76,7 @@ class TestCLIArguments(unittest.TestCase):
     @patch("pytest_watch.command.watch", side_effect=lambda *args, **kwargs: 0)
     def test_default_spool_value(self, watch_callee):
         main([])
+
         self.assertIn("spool", watch_callee.call_args[1])
         self.assertEqual(200, watch_callee.call_args[1]["spool"])
         watch_callee.assert_called_once()
