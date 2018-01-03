24edddacc1c9d230fd58fa0454a11f7a733eda20
Andre Pastore <pastore@mondli.com>
2018-01-03 14:06:38 -0200

    Added initial tests for __main__.py

---
+++ pytest_watch/tests/test_main.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import unittest
+
+try:
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
+from pytest_watch import __main__
+from pytest_watch.__main__ import run_cli
+
+
+class TestRunCLI(unittest.TestCase):
+
+    @patch("pytest_watch.command.main", side_effect=lambda argv: 0)
+    def test_add_pytest_watch_folder_to_path(self, main):
+        run_cli()
+        self.assertIn(os.path.dirname(__main__.__file__), sys.path)
+
