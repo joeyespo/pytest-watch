b0f4b1894186690f67cb119e5fbced1dd5b7fdf2
Andre Pastore <pastore@mondli.com>
2018-01-03 14:06:38 -0200

    Added initial tests for __main__.py

--- pytest_watch/__main__.py
+++ pytest_watch/__main__.py
@@ -12,6 +12,7 @@ Local continuous test runner with pytest and watchdog.
 def run_cli():
     import os
     import sys
+
     sys.path.append(os.path.dirname(__file__))
 
     from pytest_watch.command import main
