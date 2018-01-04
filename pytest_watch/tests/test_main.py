665635c897f94b40c2751a3e01c2388bdfb61867
Andre Pastore <pastore@mondli.com>
2018-01-04 16:19:31 -0200

    classless tests over simple functions
    Re: suggested by @joeyespo at https://github.com/joeyespo/pytest-watch/pull/79/files#r159579312

--- pytest_watch/tests/test_main.py
+++ pytest_watch/tests/test_main.py
@@ -11,9 +11,7 @@ from pytest_watch import __main__
 from pytest_watch.__main__ import run_cli
 
 
-class TestRunCLI(unittest.TestCase):
-
-    @patch("pytest_watch.command.main", side_effect=lambda argv=None: 0)
-    def test_add_pytest_watch_folder_to_path(self, main):
-        run_cli()
-        assert os.path.dirname(__main__.__file__) in sys.path
+@patch("pytest_watch.command.main", side_effect=lambda argv=None: 0)
+def test_add_pytest_watch_folder_to_path(main):
+    run_cli()
+    assert os.path.dirname(__main__.__file__) in sys.path
