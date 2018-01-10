ac44193f7ec9d19329b0342a26405759aac75579
Andre Pastore <pastore@mondli.com>
2018-01-10 10:42:54 -0200

    Add basic tests for watched events.

--- pytest_watch/tests/test_watcher_events.py
+++ pytest_watch/tests/test_watcher_events.py
@@ -1,23 +1,41 @@
-from watchdog.events import FileModifiedEvent, DirModifiedEvent
+from watchdog.events import FileModifiedEvent, FileMovedEvent, FileCreatedEvent, \
+     FileDeletedEvent, FileCreatedEvent, DirModifiedEvent, FileSystemEvent
 
 from pytest_watch.watcher import EventListener
 
 
-def test_unwatched_event():
-    event = DirModifiedEvent("/tmp/file.py")
-    listener = EventListener()
-
-    assert listener.event_queue.empty()
-    listener.on_any_event(event)
-
-    assert listener.event_queue.empty()
-
-
-def test_file_modified_event():
-    event = FileModifiedEvent("/tmp/file.py")
+def _assert_watched_filesystem_event(event):
     listener = EventListener()
 
     assert listener.event_queue.empty()
     listener.on_any_event(event)
 
     assert not listener.event_queue.empty()
+
+
+def _assert_unwatched_filesystem_event(event):
+    listener = EventListener()
+
+    assert listener.event_queue.empty()
+    listener.on_any_event(event)
+
+    assert listener.event_queue.empty()
+
+
+def test_unwatched_event():
+    _assert_unwatched_filesystem_event(FileSystemEvent("/tmp/file.py"))
+    _assert_unwatched_filesystem_event(DirModifiedEvent("/tmp/"))
+
+
+def test_file_modify_event():
+    _assert_watched_filesystem_event(FileModifiedEvent("/tmp/file.py"))
+
+def test_file_create_event():
+    _assert_watched_filesystem_event(FileCreatedEvent("/tmp/file.py"))
+
+def test_file_move_event():
+    _assert_watched_filesystem_event(FileMovedEvent("/tmp/file.py", "/tmp/file-new.py"))
+
+def test_file_delete_event():
+    _assert_watched_filesystem_event(FileDeletedEvent("/tmp/file.py"))
+
