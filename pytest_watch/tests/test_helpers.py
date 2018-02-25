36ccfcb07f0b8ae628314c402b53f409ab89a60a
Andre Pastore <pastore@mondli.com>
2018-02-25 17:32:07 -0300

    Added tests for helpers.send_keyboard_event, covering is_windows (True|False) cases and legacy python versions

---
+++ pytest_watch/tests/test_helpers.py
@@ -0,0 +1,82 @@
+import signal
+import subprocess
+import sys
+
+import pytest
+from pytest_watch import helpers
+
+
+@pytest.fixture
+def windows_ctrlc_mock(mocker):
+    k32_mock = mocker.patch("pytest_watch.helpers.ctypes")
+    ctrlc_mock = mocker.patch.object(k32_mock.windll.kernel32, "GenerateConsoleCtrlEvent")
+    return ctrlc_mock
+
+
+@pytest.fixture
+def python_version_proc():
+    return subprocess.Popen(f"{sys.executable}",
+                            shell=helpers.is_windows)
+
+
+def test_linux_process_kill_is_called(mocker, python_version_proc):
+    is_windows = mocker.patch.dict("pytest_watch.helpers.__dict__",
+                                   {"is_windows": False})
+
+    os_mock = mocker.patch("pytest_watch.helpers.os")
+
+    kill_mock = mocker.patch.object(os_mock, "kill",
+                                    side_effect=lambda pid, s: pid)
+
+    with python_version_proc as proc, kill_mock:
+        helpers.send_keyboard_interrupt(proc)
+
+    assert 1 == kill_mock.call_count
+    assert (proc.pid, signal.SIGINT) == kill_mock.call_args[0]
+
+
+def test_windows_process_kill_for_python26upper_is_called(mocker,
+                                                          python_version_proc,
+                                                          windows_ctrlc_mock):
+    ctrl_c_code = signal.SIGINT
+
+    is_windows = mocker.patch.dict("pytest_watch.helpers.__dict__",
+                                   {"is_windows": True})
+    ctrl_c_event = mocker.patch.dict("pytest_watch.helpers.signal.__dict__",
+                                     {"CTRL_C_EVENT": ctrl_c_code})
+
+    os_mock = mocker.patch("pytest_watch.helpers.os")
+
+    kill_mock = mocker.patch.object(os_mock, "kill",
+                                    side_effect=lambda pid, s: pid)
+
+    with kill_mock, windows_ctrlc_mock, python_version_proc as proc:
+        mocker.patch.object(proc, "wait", side_effect=print)
+        helpers.send_keyboard_interrupt(proc)
+
+    assert 0 == windows_ctrlc_mock.call_count
+    assert 1 == kill_mock.call_count
+    assert (0, ctrl_c_code) == kill_mock.call_args[0]
+
+
+def test_windows_process_kill_for_python26_is_called(mocker,
+                                                     windows_ctrlc_mock,
+                                                     python_version_proc):
+    ctrl_c_code = signal.SIGINT
+
+    is_windows = mocker.patch.dict("pytest_watch.helpers.__dict__",
+                                   {"is_windows": True})
+    ctrl_c_event = mocker.patch.dict("pytest_watch.helpers.signal.__dict__",
+                                     {"CTRL_C_EVENT": ctrl_c_code})
+
+    os_mock = mocker.patch("pytest_watch.helpers.os")
+
+    kill_mock = mocker.patch.object(os_mock, "kill",
+                                    side_effect=AttributeError)
+
+    with kill_mock, windows_ctrlc_mock, python_version_proc as proc:
+        mocker.patch.object(proc, "wait", side_effect=print)
+        helpers.send_keyboard_interrupt(proc)
+
+    assert 1 == windows_ctrlc_mock.call_count
+    assert (0, 0) == windows_ctrlc_mock.call_args[0]
