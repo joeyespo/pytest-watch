642d63d1aea2fcd17c561b9392dd2fa56b78b2a2
Andre Pastore <pastore@mondli.com>
2018-02-25 23:49:26 -0300

    Added pytest_watch.util test, covering Exception handling

--- pytest_watch/tests/test_util.py
+++ pytest_watch/tests/test_util.py
@@ -3,8 +3,15 @@ import pytest
 from pytest_watch.util import silence
 
 
-def test_handle_exception():
-    with pytest.raises(Exception) as ex:
+def test_handle_exception_type():
+    with pytest.raises(ValueError) as ex:
         with silence():
-            raise ValueError()
+            raise ValueError("Custom message error")
     assert ex.errisinstance(ValueError)
+
+
+def test_handle_exception_message():
+    with pytest.raises(KeyError) as ex:
+        with silence():
+            raise KeyError("Custom message error")
+    assert ex.match("Custom message error")
